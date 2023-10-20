350ecd560f6f8f7512815a20a6a4f89c2e6bad48
sofya-balashova <sofi.balashova@mail.ru>
2023-10-20 12:07:15 +0300

    fix V3

--- migrations/V3__Alter_table.sql
+++ migrations/V3__Alter_table.sql
@@ -35,16 +35,16 @@ ALTER TABLE pets ADD CONSTRAINT check_type_pet CHECK (type ~ '^[a-zA-Z]+$');
 
 ALTER TABLE techniques ALTER COLUMN data SET NOT NULL;
 ALTER TABLE techniques ALTER COLUMN time SET NOT NULL;
-ALTER TABLE techniques ALTER COLUMN pet SET NOT NULL;
-ALTER TABLE techniques ALTER COLUMN doctor SET NOT NULL;
-ALTER TABLE techniques ADD CONSTRAINT unique_technique UNIQUE (data, time, pet);
+ALTER TABLE techniques ALTER COLUMN pet_id SET NOT NULL;
+ALTER TABLE techniques ALTER COLUMN doctor_id SET NOT NULL;
+ALTER TABLE techniques ADD CONSTRAINT unique_technique UNIQUE (data, time, pet_id);
 ALTER TABLE techniques ADD CONSTRAINT check_time_techniques CHECK (time >= '09:00:00' AND time <= '18:00:00');
 
 ALTER TABLE diagnoses ALTER COLUMN name SET NOT NULL;
 ALTER TABLE diagnoses ALTER COLUMN description SET NOT NULL;
 ALTER TABLE diagnoses ALTER COLUMN name SET NOT NULL;
 ALTER TABLE diagnoses ALTER COLUMN complications SET NOT NULL;
-ALTER TABLE diagnoses ADD CONSTRAINT unique_diagnosis UNIQUE (name, patient);
+ALTER TABLE diagnoses ADD CONSTRAINT unique_diagnosis UNIQUE (name, patient_id);
 
 ALTER TABLE medicaments ALTER COLUMN name SET NOT NULL;
 ALTER TABLE medicaments ALTER COLUMN manual SET NOT NULL;
